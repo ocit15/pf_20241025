5cbcea2c413c1f2d1b8625930e85a6ff8b807835
unknown <juasruhiyatsi55@gmail.com>
2022-11-03 10:32:32 +0800

    kuliah hari ini 03/11/2022

---
+++ Pertemuan2/index.php
@@ -0,0 +1,37 @@
+<?php  
+//standar output di php
+//echo,print,print-f,var_dump
+
+var_dump(" Hello World!") ;
+
+//variabel
+// $nama = "M.CITRA SATRIA";
+// echo "Nama saya $nama";
+
+//operator
+//aritmatika
+// * / + - %
+
+// $x = 10;
+// $y = 20;
+// echo $x * $y ;
+// echo $x + $y ;
+// echo $x / $y ;
+// echo $x - $y ;
+
+//penggabungan string/concat
+$nama_depan ="ocit";
+$nama_belakang ="sat";
+// echo $nama_depan . " " . $nama_belakang;
+
+//asigment
+$x = 1;
+$x += 5;
+echo $x;
+
+$nama = "";
+$nama .= $nama_depan;
+$nama .= " ";
+$nama .= $nama_belakang;
+echo $nama;
+?>
