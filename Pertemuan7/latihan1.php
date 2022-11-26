acd162415d9541000cde62f058f6318bf97d38a0
ocit15 <117325699+ocit15@users.noreply.github.com>
2022-11-26 15:42:58 +0800

    Add files via upload

---
+++ Pertemuan7/latihan1.php
@@ -0,0 +1,19 @@
+<?php 
+  // variabel scope / lingkup variabel
+  //$x = 10; // $x merupakan variabel global untuk 1 file saja
+
+  // function tampilX(){
+  //   $x = 20; // $x adalah variabel untuk function tampilX() saja
+  //   echo $x;
+  // }
+
+  // tampilX();
+
+  // SUPERGLOBALS
+  // variabel global milik PHP
+  // merupakan Array Associative
+  echo $_SERVER["SERVER_NAME"];
+
+  
+
+?>
